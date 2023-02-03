d0442d43954985721394c26b1542fed1bb0a50a7
simonecossaro <105944449+simonecossaro@users.noreply.github.com>
2023-02-03 16:48:22 +0100

    Update MovieGenius-home.py

--- MovieGenius-home.py
+++ MovieGenius-home.py
@@ -6,26 +6,7 @@ def recommendationpage():
    placeholder3 = st.empty()
    with placeholder3.container():
     st.write('Film raccomandati')
-    st.radio( "What do you want to feel by watching the movie?" , mood_list, key="asdfgh")
     b3 = st.button('exit')
-
-def childpage():
-   placeholder1 = st.empty()
-   with placeholder1.container():
-    st.write('Ciao bambino')
-    
-def adultpage():
-   placeholder2 = st.empty()
-   with placeholder2.container():
-    st.radio( "What do you want to feel by watching the movie?" , mood_list, key="asdfgh")
-        
-    st.text_input('Here you can name a movie similar to the one you want to watch (*optional*)', key="zxcvbn")
-                      
-    st.radio("How much time do you have?", ["infinite","limited"], key="minutes")
-                      
-    if st.button('Go to recommendations'):
-         placeholder2.empty()
-         recommendationpage()
     
 
 mood_list = ["laugh", "cry","love","adventure","fear","adrenaline","fantasy","science fiction","random"]
@@ -38,14 +19,26 @@ with placeholder.container():
              ''' , unsafe_allow_html=True)
   st.radio( "Are you an adult or a child?" , ["adult", "child"], key="qwerty")
   b = st.button('Next')
-  
-if (b and st.session_state.qwerty == "child"):
-    placeholder.empty()
-    childpage()
+  if (b and st.session_state.qwerty == "child"):
+   st.text_input('Here you can name a movie similar to the one you want to watch (*optional*)', key="zxcvbn")                   
+   st.radio("How much time do you have?", ["infinite","limited"], key="minutes")
+   if st.button('Go to recommendations'):
+      placeholder.empty()
+      recommendationpage()
+  if (b and st.session_state.qwerty == "adult"):
+   st.radio( "What do you want to feel by watching the movie?" , mood_list, key="asdfgh")
+   st.text_input('Here you can name a movie similar to the one you want to watch (*optional*)', key="zxcvbn")            
+   st.radio("How much time do you have?", ["infinite","limited"], key="minutes")
+   if st.button('Go to recommendations'):
+      placeholder.empty()
+      recommendationpage()
+      
     
-if (b and st.session_state.qwerty == "adult"):
-    placeholder.empty()
-    adultpage()
+  
+
+    
+    
+
                      
                       
                 
