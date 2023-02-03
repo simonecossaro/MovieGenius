99e28bbf7f313cdfce3fdcb8305069250c699048
simonecossaro <105944449+simonecossaro@users.noreply.github.com>
2023-02-03 17:31:20 +0100

    Update MovieGenius-home.py

--- MovieGenius-home.py
+++ MovieGenius-home.py
@@ -2,13 +2,21 @@ import streamlit as st
 
 st.set_page_config(page_title="MovieGenius")
 
-def recommendationpage():
+def childpage():
     placeholder3 = st.empty()
     with placeholder3.container():
-        st.write('Film raccomandati')
-        b3 = st.button('exit')
-    
-r = False
+        st.text_input('Here you can name a movie similar to the one you want to watch (*optional*)', key="zxcvbn")                   
+        st.radio("How much time do you have?", ["infinite","limited"], key="minutes")
+        b1 = st.button('Go to recommendations')
+        if b1:
+            placeholder3.empty()
+            recommendationpage()
+            
+def recommendationpage():
+    placeholder4 = st.empty()
+    with placeholder4.container():
+        st.write('Ecco i film raccomandati')
+            
 mood_list = ["laugh", "cry","love","adventure","fear","adrenaline","fantasy","science fiction","random"]
 
 st.write(f'''
@@ -22,7 +30,7 @@ with placeholder.container():
 
 if (b and st.session_state.qwerty == "child"):
     placeholder.empty()
-    recommendationpage()
+    childpage()
                     
             
                 
