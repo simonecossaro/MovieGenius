d2684675c4956e01f1b69dab6d4f46a78d0cf2ba
simonecossaro <105944449+simonecossaro@users.noreply.github.com>
2023-02-03 18:10:59 +0100

    Update MovieGenius-home.py

--- MovieGenius-home.py
+++ MovieGenius-home.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import MovieRecommendationSystem as mr
+import DatasetInspection as di
+import pandas as pd
+import numpy as np
 
 st.set_page_config(page_title="MovieGenius")
 
@@ -21,11 +24,36 @@ with placeholder.container():
     
 if b:
     placeholder.empty()
-    st.write(st.session_state['tipo'])
-    st.write(st.session_state['mood'])
-    st.write(st.session_state['film_target'])
-    st.write(st.session_state['time'])
-    st.write(st.session_state['minutes'])
+    for_child = False
+    if (st.session_state['tipo'] == 'child'):
+            for_child = True
+    tMax = 600
+    if (st.session_state['time'] == "limited"):
+            tMax = st.session_state['minutes']
+    m = mr.MovieRecommendationSystem()
+    recommended = m.recommend(for_kids, st.session_state['mood'], st.session_state['film_target'], tMax )
+    listRecom = recommended['title'].values
+    listOverview = []
+    st.write(f'''
+                 <h1> Movie recommendation App </h1><h2> Recommended movies: </h2>
+             ''' , unsafe_allow_html=True)
+    for i in range(len(listRecom)):
+            value = di.trovaOverview(listRecom[i])
+            listOverview.append(value)
+    col1, col2 = st.columns(2)
+    with col1:
+            for i in range(0,10):
+                        st.write(f'''
+                                     <div > <big><b>%d -</b>  %s </big>
+                                    </div>
+                                    ''' % (i+1, listRecom[i]), unsafe_allow_html=True)
+    with col2:
+             option= st.selectbox("Read the overview of", (listRecom[0],listRecom[1],listRecom[2],listRecom[3],listRecom[4],listRecom[5],listRecom[6],
+                                                     listRecom[7],listRecom[8],listRecom[9]))
+             st.write(di.trovaOverview(option))
+        
+   
+                        
          
 
      
