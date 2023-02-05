eb9c2255120b07cbc21c37e5c9ebaa7e9bf49201
simonecossaro <105944449+simonecossaro@users.noreply.github.com>
2023-02-05 15:55:34 +0100

    Update output.py

--- output.py
+++ output.py
@@ -5,7 +5,17 @@ import streamlit as st
 
 st.write('Recommended movies')
 
-st.write(st.session_state.qwerty)
-st.write(st.session_state.asdfgh)
-st.write(st.session_state.zxcvbn)
-st.write(st.session_state.time)
+values = st.experimental_get_query_params()["qwerty"][0]
+
+
+list1 = values.split('/?')
+
+child = list1[0].replace("qwerty=", "")
+mood = list1[1].replace("asdfgh=", "")
+film_target = list1[2].replace("zxcvbn=", "").replace("%20", " ")
+time = list1[3].replace("time=", "")
+
+st.write(child)
+st.write(mood)
+st.write(film_target)
+st.write(time)
