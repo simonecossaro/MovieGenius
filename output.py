6632eb753f7d17a587b5766fd8f221f45514e89c
simonecossaro <105944449+simonecossaro@users.noreply.github.com>
2023-02-05 15:59:44 +0100

    Update output.py

--- output.py
+++ output.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import MovieRecommendationSystem as mr
 import streamlit as st
+import DatasetInspection as di
+import numpy as np
 
 
 st.write('Recommended movies')
@@ -19,3 +21,41 @@ st.write(child)
 st.write(mood)
 st.write(film_target)
 st.write(time)
+
+for_kids = False
+if (child == 'child'):
+  for_kids = True
+  
+m = mrs.MovieRecommendationSystem()
+recommended = m.recommend(for_kids, mood, film_target , Tmax )
+
+listRecom = recommended['title'].values
+
+
+listOverview = []
+
+st.write(f'''
+                 <h1> Movie recommendation App </h1><h2> Recommended movies: </h2>
+             ''' , unsafe_allow_html=True)
+
+
+for i in range(len(listRecom)):
+    value = di.trovaOverview(listRecom[i])
+    listOverview.append(value)
+    
+        
+col1, col2 = st.columns(2)
+
+with col1:
+    for i in range(0,10):
+           st.write(f'''
+                <div > <big><b>%d -</b>  %s </big>
+                </div>
+                ''' % (i+1, listRecom[i]), unsafe_allow_html=True)
+
+    
+with col2:
+        
+    option= st.selectbox("Read the overview of", (listRecom[0],listRecom[1],listRecom[2],listRecom[3],listRecom[4],listRecom[5],listRecom[6],
+                                                     listRecom[7],listRecom[8],listRecom[9]))
+    st.write(di.trovaOverview(option))
