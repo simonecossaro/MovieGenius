3c70afa649fc5b9bf7ee7375abbb74d1f9021fcf
simonecossaro <105944449+simonecossaro@users.noreply.github.com>
2023-02-05 16:02:47 +0100

    Update output.py

--- output.py
+++ output.py
@@ -4,9 +4,6 @@ import streamlit as st
 import DatasetInspection as di
 import numpy as np
 
-
-st.write('Recommended movies')
-
 values = st.experimental_get_query_params()["qwerty"][0]
 
 
@@ -16,11 +13,6 @@ child = list1[0].replace("qwerty=", "")
 mood = list1[1].replace("asdfgh=", "")
 film_target = list1[2].replace("zxcvbn=", "").replace("%20", " ")
 time = list1[3].replace("time=", "")
-
-st.write(child)
-st.write(mood)
-st.write(film_target)
-st.write(time)
 
 for_kids = False
 if (child == 'child'):
@@ -37,7 +29,7 @@ listRecom = recommended['title'].values
 listOverview = []
 
 st.write(f'''
-                 <h1> Movie recommendation App </h1><h2> Recommended movies: </h2>
+                 <h1> Movie Genius </h1><h2> Recommended movies: </h2>
              ''' , unsafe_allow_html=True)
 
 
