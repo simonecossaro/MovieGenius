0c9ca3925c8986e25675b65f008d5149ed04505a
simonecossaro <105944449+simonecossaro@users.noreply.github.com>
2023-02-05 17:04:58 +0100

    Update MovieGenius-home.py

--- MovieGenius-home.py
+++ MovieGenius-home.py
@@ -73,8 +73,28 @@ def goToPage(mood, movie, time):
                         nav_to("https://simonecossaro-moviegenius-output-mv1sij.streamlit.app")
                     
 ############################################################################
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+#function to set the background
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+                    <style>
+                        .stApp {
+                            background-image: url("data:image/png;base64,%s");
+                            background-size: cover;
+                        }
+                    </style>
+                  ''' % (bin_str)
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+############################################################################
 
 st.set_page_config(page_title="🎬 Movie Genius", page_icon="🎬")
+
+set_background()
 
             
 mood_list = ["laugh", "cry","love","adventure","fear","adrenaline","fantasy","science fiction","random"]
